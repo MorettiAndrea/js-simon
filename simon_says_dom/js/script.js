ce375e70e042d0e4cc10754c2c554347c452052a
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-13 00:48:05 +0200

    creazione timer

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,7 +1,7 @@
 // IDs
 
-const coundown = document.getElementById("countdown");
-const instructions = document.getElementById("instruction");
+const countdown = document.getElementById("countdown");
+const instructions = document.getElementById("instructions");
 const numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 
@@ -20,3 +20,28 @@ function fromRandomToArray() {
 }
 
 console.log(fromRandomToArray());
+
+// creazione timer
+
+let timeLeftMs = 3000;
+let countdownIntervalId;
+
+// condizioni fine countdown
+const timeIsOver = () => {
+  if (timeLeftMs === 0) {
+    countdown.style.display = "none";
+    instructions.style.display = "none";
+    answersForm.classList.remove("d-none");
+    clearInterval(countdownIntervalId);
+  }
+  return timeIsOver;
+};
+const timerStart = () => {
+  timeLeftMs -= 1000;
+  countdownIntervalId = setInterval(timeLeftMs, 1000);
+  return countdownIntervalId;
+};
+timeIsOver();
+timerStart();
+console.log(timeIsOver);
+console.log(timerStart);
