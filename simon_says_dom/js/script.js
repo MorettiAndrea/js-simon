2e4c07688088439eec964a9e4b79d0c7e8637ddd
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-07-16 10:40:04 +0200

    done

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -4,10 +4,18 @@ const countdown = document.getElementById("countdown");
 const instructions = document.getElementById("instructions");
 const numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
+const formButton = document.getElementById("form-button")
+ const inputs = document.querySelectorAll("input");
+const message = document.getElementById("message")
+const resetBtn = document.getElementById("reset-btn")
 
-// funzione numero casuale
+
+// variabili generali
+let generatedNumbers = []
+let playerNumbers= []
 
 // elementi timer
+
 let timeLeft = 3;
 
 const clock = setInterval(() => {
@@ -22,11 +30,15 @@ const timeIsOver = () => {
   countdown.style.display = "none";
   instructions.style.display = "none";
   answersForm.classList.remove("d-none");
-numbersList.classList.add("d-none");
+  numbersList.classList.add("d-none");
   clearInterval(clock);
 };
 
-let generatedNumbers = []
+
+
+
+
+// funzione numero casuale
 
 const randomNumbersGenerator = (min,max)=>{
   const rng = Math.floor(Math.random() *(max-min+1)+min)
@@ -36,14 +48,56 @@ const randomNumbersGenerator = (min,max)=>{
 
   for (let i = 0; generatedNumbers.length < 5;){
     
-    const randomNumber = randomNumbersGenerator(1,10);
+    const randomNumber = randomNumbersGenerator(1,50);
     if(!generatedNumbers.includes(randomNumber))
    { generatedNumbers.push(randomNumber);
   i++}
    }
 
-   console.log(generatedNumbers);
-
+ 
    const numbersIntoList = generatedNumbers.map((randomNumber)=>`<li>${randomNumber}</li>`)
    
    numbersList.innerHTML = numbersIntoList.join("")
+
+   formButton.addEventListener("click",(e)=>
+ { e.preventDefault(); 
+
+  for (let i = 0; i<inputs.length;i++){
+    const currentInput= inputs[i]
+    const inputValue = parseInt(currentInput.value)
+    if(!playerNumbers.includes(inputValue))
+      { playerNumbers.push(parseInt(inputValue));}
+    else{alert("non puoi inserire due numeri uguali"); 
+      playerNumbers=[];
+      return}
+  }
+ 
+  const matchingNumbers = []
+  for(const number of generatedNumbers)
+    {
+    if(playerNumbers.includes(number))
+      {matchingNumbers.push(number)}
+   
+  }
+ resetBtn.classList.remove("d-none")
+    resetBtn.classList.add("d-flex")
+    resetBtn.addEventListener("click",(e)=>{
+      location.reload()
+    })
+
+matchingNumbers.length ? message.innerHTML=`Complimenti! hai indovinato i numeri ${matchingNumbers.join(",")}`: message.innerHTML=`Nessun numero indovinato`
+
+
+
+
+  console.log(generatedNumbers);
+   console.log(playerNumbers);
+   console.log(matchingNumbers);
+
+  })
+   
+  
+
+
+  
+   
